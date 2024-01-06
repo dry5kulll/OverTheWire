2073a64c00077e16286cddfce86b45b91b4ea76f
d4y5kulll88 <148268845+d4y5kulll88@users.noreply.github.com>
2024-01-06 19:01:55 +0530

    Update Natas 3.py

--- Natas 3.py
+++ Natas 3.py
@@ -1,19 +1,28 @@
+# Vulnerability: Sensitive information exposed in robots.txt file
+
 import requests
 import re
 import colorama
 from colorama import Fore, Style
 
+
+# Initialize colorama for colored console output
 colorama.init(autoreset=False)
 print(Fore.LIGHTBLUE_EX, Style.BRIGHT)
 
+
+# Set the username and password for authentication
 username = 'natas3'
 password = 'G6ctbMJ5Nb4cbFwhpMPSvxGHhQ7I6W8Q'
 
+
+# Construct the URL using the given username
 url = f'http://{username}.natas.labs.overthewire.org/s3cr3t/users.txt'
 
+
+# Make a GET request to the URL with basic authentication using the provided credentials
 resp = requests.get(url=url, auth=(username, password))
 
+
+# Extract the password for the next level using Regular Expressions
 print(f"Natas4: {re.findall('natas4:(.*)', resp.text)[0]}")
-
-
-
