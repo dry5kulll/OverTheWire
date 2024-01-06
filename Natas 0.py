9e5f3bd5445124803982426dd3886876dccade74
d4y5kulll88 <148268845+d4y5kulll88@users.noreply.github.com>
2024-01-06 18:59:58 +0530

    Update Natas 0.py

--- Natas 0.py
+++ Natas 0.py
@@ -1,22 +1,39 @@
-# Sensitive information disclosure in the source code
+# Vulnerability: Sensitive information disclosure in the source code
 
 import requests
 import re
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 import colorama
 from colorama import Fore, Style
 
+
+# Initialize colorama for colored console output
 colorama.init(autoreset=False)
 print(Fore.LIGHTBLUE_EX, Style.BRIGHT)
 
+
+# Set the username and password for authentication
 username = 'natas0'
 password = username
 
+
+# Construct the URL using the given username
 url = f'http://{username}.natas.labs.overthewire.org'
 
+
+# Make a GET request to the URL with basic authentication using the provided credentials
 resp = requests.get(url=url, auth=(username, password))
 
+
+# Parse the HTML content of the response using BeautifulSoup
 soup = BeautifulSoup(resp.text, 'lxml')
-# print(soup.prettify())
-# print(soup)
-print(re.findall('<!--(.*) -->', resp.text)[-1])
+
+
+# Extract password for the next level
+# Method 1: BeautifulSoup Approach using Comment class
+comment_tag = soup.find_all(string=lambda text: isinstance(text, Comment))[-1]
+print(comment_tag.strip())
+
+
+# Method 2: Regular Expressions
+# print(re.findall('<!--(.*) -->', resp.text)[-1])
