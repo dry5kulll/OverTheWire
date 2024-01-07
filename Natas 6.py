0b8c96799fa53b5b6dc4f60a96279714ce3dd4a0
d4y5kulll88 <148268845+d4y5kulll88@users.noreply.github.com>
2024-01-07 12:35:35 +0530

    Update Natas 6.py

--- Natas 6.py
+++ Natas 6.py
@@ -1,27 +1,37 @@
+# Vulnerability: Secret key file location exposed in source code
+
 import requests
 import re
-import html
 from bs4 import BeautifulSoup
 import colorama
 from colorama import Fore, Style
 
+
+# Initialize colorama for colored console output
 colorama.init(autoreset=False)
 print(Fore.LIGHTBLUE_EX, Style.BRIGHT)
 
-rep_list = {'<br/>': '\n', '&nbsp;': ' ', '&amp;': '&'}
 
+# Set the username and password for authentication
 username = 'natas6'
 password = 'fOIvE0MDtPTgRhqmmvvAOt2EfXR6uQgR'
-data = {"secret": "FOEIUWGHFEEUHOFUOIU", "submit": "What Ever itr can be"}
 
+
+# Secret key to unlock the password for the next level
+data = {"secret": "FOEIUWGHFEEUHOFUOIU", "submit": "What Ever it can be"}
+
+
+# Construct the URL using the given username
 url = f'http://{username}.natas.labs.overthewire.org/'
 
+
+# Make a POST request to the URL with basic authentication using the provided credentials
 resp = requests.post(url=url, auth=(username, password), data=data)
 
-soup = BeautifulSoup(html.unescape(resp.text), 'lxml')
-for key, value in rep_list.items():
-    soup = re.sub(key, value, str(soup))
 
-soup = BeautifulSoup(soup, 'lxml')
+# Parse the HTML content of the response using BeautifulSoup
+soup = BeautifulSoup(resp.text, 'lxml')
 
+
+# Extract password for the next level using Regular Expressions
 print(re.findall('Access granted. (.*)', soup.text)[0])
